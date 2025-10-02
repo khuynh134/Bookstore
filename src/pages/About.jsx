f2eb1c189778bffb62ca66b369f590f36ee52e2c
Kathy Huynh <khuynh134@example.com>
2025-10-01 21:33:47 -0700

    reformatted the container for About Us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,8 @@ function About() {
     return (
         <div className="about-container">
             <h1 className="About-title">About Us </h1>
-            <p className="About-description"> Our team members are Kathy Huynh, Ying Huang, and Valeria Campos</p>
+            <h2 className="About-subtitle"> Our team members: Kathy Huynh, Ying Huang, and Valeria Campos</h2>
+            <p className="About-description"> Welcome to our bookstore websitewith a large collection of books you can view and choose to add to your cart. </p>
 
         </div>
     );
