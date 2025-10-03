34222267ff6b68c9d0cc6500f8c3f065662e8ead
khuynh134 <khuynh134@csu.fullerton.edu>
2025-10-02 19:40:19 -0700

    added some descriptions to home page and about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,9 @@ function About() {
         <div className="about-container">
             <h1 className="About-title">About Us </h1>
             <h2 className="About-subtitle"> Our team members: Kathy Huynh, Ying Huang, and Valeria Campos</h2>
-            <p className="About-description"> Welcome to our bookstore websitewith a large collection of books you can view and choose to add to your cart. </p>
+            <p className="About-description"> Welcome to our website! Are you interested in exploring different books? 
+                We have a large collection of books that is perfect for book lovers like you. Feel free to browse throught our collection and add your favorite books to your cart.
+                </p>
 
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import './Home.css';
 function Home() {
     return (
         <div className="home-container">
-            <h1 className="home-title">Welcome to the Home Page</h1>
-            <p className="home-description">Discovery the amazing inventory of books we have!</p>
+            <h1 className="home-title">Hello, Nice to meet you!</h1>
+            <p className="home-description">Explore the amazing inventory of books we have!</p>
 
 
         </div>
