9bd748e4a2a2f61ce092f6da15c160c793f9aa05
valeria-campos98 <valeria.campos2055@gmail.com>
2025-10-04 01:14:10 -0700

    testing
    making a few changes to test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import './Home.css';
 function Home() {
     return (
+        <div>
         <div className="home-container">
             <h1 className="home-title">Hello, Nice to meet you!</h1>
             <p className="home-description">Explore the amazing inventory of books we have!</p>
 
-
         </div>
+        <div className="parallax"></div>  
+     </div>
     );
 }
 
