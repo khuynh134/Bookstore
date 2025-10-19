035bdf4154d6c0672625b387dd2b726942f1e7a0
valeria-campos98 <valeria.campos2055@gmail.com>
2025-10-19 14:28:19 -0700

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,30 @@
-import './Cart.css'; 
+import './Cart.css';
 
 function Cart() {
-    return (
-        <h1> Your Cart </h1> 
-    )
+  return (
+    <div>
+      <h1>Your Cart</h1>
+
+      <section id="cart" className="section-p1">
+        <table width="100%">
+          <thead>
+            <tr>
+              <td>Remove</td>
+              <td>Image</td>
+              <td>Product</td>
+              <td>Quantity</td>
+              <td>Subtotal</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><i className="far fa-times-circle"></i></td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
 
 export default Cart;
