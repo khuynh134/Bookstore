35d5a7c563962905481c3a6f4aeae37fd55be284
YingHuang54 <yinghuang1517977905@gmail.com>
2025-11-16 00:36:24 -0800

    Style change to BookWithAddToCart, Pagination, and SearchResult

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,10 +3,10 @@ import './Pagination.css';
 export default function Pagination({page, setPage, hasNext}){
     return (
         <div className='pagination'>
-            {(page != 1) ? <button className="prev-btn" onClick={()=>{setPage(page - 1)}}>Prev</button> 
+            {(page != 1) ? <button className="prev-btn" onClick={()=>{setPage(page - 1)}}>{"<"}</button> 
             : <div></div>}
             <span className="page-number">page <strong>{page}</strong></span>
-            {hasNext? <button className="next-btn" onClick={()=>{setPage(page + 1)}}>Next</button>
+            {hasNext? <button className="next-btn" onClick={()=>{setPage(page + 1)}}>{">"}</button>
             : <div></div>}
         </div>
     )
